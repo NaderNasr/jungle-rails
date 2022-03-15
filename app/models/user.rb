de4589f6e58fabe4edab8ced39b529fb1033a316
NaderNasr <nadernasr7@gmail.com>
2022-03-15 12:39:07 -0400

    remove white space and convert email string to lower case method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, length: { maximum: 30 }, presence: true
+  validates :last_name, length: { maximum: 30 }, presence: true
+  validates :email, case_sensitive: false, presence: true
+  validates :password, length: { minimum: 3 }, presence: true
+  validates :password_confirmation, length: { minimum: 3 }, presence: true
+
+  def self.authenticate_with_credentials email, password
+    # '     test@TEST.com ' => 'test@test.com'
+    formatedEmail = email.downcase.strip
+    # if user == email && password is authenticated
+    if User.find_by_email(formatedEmail)&.authenticate(password)
+      # sign in with user email
+      User.find_by_email(formatedEmail)
+    else
+      nil
+    end
+  end
 end
